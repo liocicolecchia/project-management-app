817aa1128f44104950a3b17115ba0d8a10dc1f6f
Lio <liocicolecchia@gmail.com>
2026-05-26 09:37:38 +0200

    adding future add new Project component & Input

---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input({ label, isTextarea, ...props }) {
+  return (
+    <p>
+      <label>{label}</label>
+      {isTextarea ? <textarea {...props} /> : <input {...props} />}
+    </p>
+  );
+}
+
+export default Input;
---
+++ src/components/NewProject.jsx
@@ -0,0 +1,23 @@
+import Input from "./Input";
+
+function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <Input label="Title" />
+        <Input label="Description" isTextarea />
+        <Input label="Due Date" />
+      </div>
+    </div>
+  );
+}
+
+export default NewProject;
