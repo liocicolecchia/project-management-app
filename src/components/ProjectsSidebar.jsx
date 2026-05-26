333c382602456468432431ea62c08e7b1bf3d3d7
Lio <liocicolecchia@gmail.com>
2026-05-27 01:05:10 +0200

    adding functionality to look at a created project

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,6 +1,11 @@
 import Button from "./Button";
 
-function ProjectsSidebar({ onStartAddProject, projects }) {
+function ProjectsSidebar({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  selectedProjectId,
+}) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -10,13 +15,27 @@ function ProjectsSidebar({ onStartAddProject, projects }) {
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul className="mt-8">
-        {projects.map((projects) => (
-          <li key={projects.id}>
-            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
-              {projects.title}
-            </button>
-          </li>
-        ))}
+        {projects.map((projects) => {
+          let cssClasses =
+            "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
+
+          if (projects.id === selectedProjectId) {
+            cssClasses += " bg-stone-800 text-stone-200";
+          } else {
+            cssClasses += " text-stone-400";
+          }
+
+          return (
+            <li key={projects.id}>
+              <button
+                onClick={() => onSelectProject(projects.id)}
+                className={cssClasses}
+              >
+                {projects.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
